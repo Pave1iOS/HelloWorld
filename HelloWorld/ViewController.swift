d932040ae75f65e774df6efe71645a1d7aff4b2d
Pavel Gribachev <PGribachev@yandex.ru>
2023-11-01 16:34:05 +0300

    Prepare project. Delete all templates comments

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -1,17 +1,11 @@
-//
-//  ViewController.swift
-//  HelloWorld
-//
-//  Created by Pavel Gribachev on 01.11.2023.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
 
